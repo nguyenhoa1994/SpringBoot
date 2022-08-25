e7d050d6d38e378adb87eb36719f6f7e05c7e0d7
hoanv <nguyenvanhoasn1994@gmail.com>
2022-08-25 14:34:56 +0700

    update file test_v4

--- src/main/java/entity/test_v4.java
+++ src/main/java/entity/test_v4.java
@@ -2,4 +2,8 @@ package entity;
 
 public class test_v4 {
     private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+    private String field5;
 }
